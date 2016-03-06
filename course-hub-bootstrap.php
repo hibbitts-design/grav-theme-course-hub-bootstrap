23a427221d1c72928679be50e099b29c96373bba
Paul Hibbitts <paul@paulhibbitts.com>
2016-03-06 08:13:11 -0800

    Updated Bootstrapper status checks.

--- course-hub-bootstrap.php
+++ course-hub-bootstrap.php
@@ -7,13 +7,19 @@ class CourseHubBootstrap extends Theme
 {
   public function onThemeInitialized() {
     // Check for Bootstrapper plugin status
-    if (!$this->grav['config']->get('plugins.bootstrapper.enabled') ||
-        !$this->grav['config']->get('plugins.bootstrapper.always_load') ||
-        $this->grav['config']->get('plugins.bootstrapper.load_theme_css')) {
+    if (!$this->grav['config']->get('plugins.bootstrapper.enabled') &&
+        $this->grav['config']->get('plugins.bootstrapper.always_load') &&
+        !$this->grav['config']->get('plugins.bootstrapper.load_theme_css')) {
       echo '<script language="javascript">';
-      echo 'alert("Please set the following Bootstrapper plugin options before using this theme:\n\nPlugin status: Enabled\nAlways load: Enabled\nLoad Theme CSS: Disabled")';
+      echo 'alert("Please enable the Bootstrapper plugin before using this theme.")';
       echo '</script>';
-    }
+    } elseif (!$this->grav['config']->get('plugins.bootstrapper.enabled') ||
+          !$this->grav['config']->get('plugins.bootstrapper.always_load') ||
+          $this->grav['config']->get('plugins.bootstrapper.load_theme_css')) {
+        echo '<script language="javascript">';
+        echo 'alert("Please set the following Bootstrapper plugin options before using this theme:\n\nPlugin status: Enabled\nAlways load: Enabled\nLoad Theme CSS: Disabled")';
+        echo '</script>';
+      }
   }
 }
 ?>
