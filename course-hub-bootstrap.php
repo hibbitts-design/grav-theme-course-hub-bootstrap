20758c37589e185178af28eaaeafb582e9025918
Paul Hibbitts <paul@paulhibbitts.com>
2016-03-11 09:52:13 -0800

    External links plugin check message updated.

--- course-hub-bootstrap.php
+++ course-hub-bootstrap.php
@@ -9,7 +9,7 @@ class CourseHubBootstrap extends Theme
     // Check for External_Links plugin status
     if ($this->grav['config']->get('plugins.external_links.built_in_css')) {
       echo '<script language="javascript">';
-      echo 'alert("Please set Use Built-in CSS for the External Links plugin to No (i.e. False) before using this theme.")';
+      echo 'alert("Please set Use Built-in CSS option for the External Links plugin to No (i.e. False) before using this theme.")';
       echo '</script>';
     }
     // Check for Bootstrapper plugin status
